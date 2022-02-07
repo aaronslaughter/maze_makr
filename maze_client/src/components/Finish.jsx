a4962c2fffaa1314ddad5b4a2fa167a3a2ca9a7e
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-07 16:45:17 -0500

    added unique components for start, finish, and border walls

---
+++ maze_client/src/components/Finish.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Finish = () => {
+  return (
+    <Div>F</Div>
+  )
+}
+
+export default Finish;
+
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  background-color: green;
+  outline: 1px solid black;
+  width: 1.5em;
+  height: 1.5em;
+`
