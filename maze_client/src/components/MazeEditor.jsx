1ef0e9440b994bddb588d8b8eb26131a3a96b2f0
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-07 16:42:34 -0500

    added start, finish, and fixed wall components

--- maze_client/src/components/MazeEditor.jsx
+++ maze_client/src/components/MazeEditor.jsx
@@ -2,28 +2,49 @@ import React from 'react';
 import styled from 'styled-components'
 import EmptyCell from './EmptyCell';
 import Wall from './Wall';
+import FixedWall from './FixedWall';
+import Start from './Start';
+import Finish from './Finish';
 
 const MazeEditor = ({maze, setMaze}) => {
 
   const handleClick = (row, col) => {
-    let tempGrid = maze.grid;
-    tempGrid[row][col] = tempGrid[row][col] ? false : true
-    setMaze({...maze, grid: tempGrid})
+    // excludes border rows and columns
+    if (row !== 0 && row !== maze.height - 1 &&
+      col !==0 && col !== maze.width - 1) {
+
+      let tempGrid = maze.grid;
+      tempGrid[row][col] = tempGrid[row][col] ? false : true
+      setMaze({...maze, grid: tempGrid})
+    }
+  }
+
+  const renderCell = (row, col) => {
+    if (row === 0 && col === 1) {
+      return <Start></Start>
+    } else if (row === maze.height - 1 && col === maze.width - 2){
+      return <Finish></Finish>
+    } else if (row === 0 || row === maze.height - 1) {
+      return <FixedWall></FixedWall>
+    } else if (col === 0 || col === maze.width - 1) {
+      return <FixedWall></FixedWall>
+    } else if (maze.grid[row][col]) {
+      return <EmptyCell></EmptyCell>
+    } else {
+      return <Wall></Wall>
+    }
   }
 
   return (
     <Container>
-      {maze.grid.map((row, rowIndex) => 
-        <div key={rowIndex}>
-          {row.map((column, colIndex) =>
+      {maze.grid.map((row, colIndex) => 
+        <div key={colIndex}>
+          {row.map((column, rowIndex) =>
             <div 
-              key={colIndex}
+              key={rowIndex}
               onMouseDown={() => handleClick(rowIndex, colIndex)}
             >
-              {maze.grid[rowIndex][colIndex] ? 
-                <EmptyCell/> : 
-                <Wall/>
-              }
+              {renderCell(rowIndex, colIndex)}
             </div>
           )}
         </div>
