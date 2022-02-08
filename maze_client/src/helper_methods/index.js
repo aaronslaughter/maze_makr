c26e53c35cceb12dd8353de70fe8efef302f36c5
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-08 10:49:03 -0500

    changed all the .push() to [][]=

--- maze_client/src/helper_methods/index.js
+++ maze_client/src/helper_methods/index.js
@@ -1,25 +1,28 @@
 export const generateMaze = (maze) => {
   let tempGrid = []
 
-    for (let i = 0; i < maze.width; i++) {
-      tempGrid.push([])
+  for (let i = 0; i < maze.height; i++) {
+    tempGrid.push([])
+    for (let j = 0; j < maze.width; j++) {
+      tempGrid[i].push([])
     }
+  }
 
-    for (let i = 0; i < maze.width; i++) {
-      for (let j = 0; j < maze.height; j++) {
-        if (i === 0 && j === 1) {
-          tempGrid[i].push(true)
-        } else if (i === maze.height - 1 && j === maze.width - 2) {
-          tempGrid[i].push(true)
-        }else if (i === 0 || i === maze.width - 1) {
-          tempGrid[i].push(false)
-        } else if (j === 0 || j === maze.height - 1) {
-          tempGrid[i].push(false)
-        } else {
-          tempGrid[i].push(true)
-        }
+  for (let i = 0; i < maze.height; i++) {
+    for (let j = 0; j < maze.width; j++) {
+      if (i === 0 && j === 1) {
+        tempGrid[i][j] = true
+      } else if (i === maze.height - 1 && j === maze.width - 2) {
+        tempGrid[i][j] = true
+      } else if (i === 0 || i === maze.height - 1) {
+        tempGrid[i][j] = false
+      } else if (j === 0 || j === maze.width - 1) {
+        tempGrid[i][j] = false
+      } else {
+        tempGrid[i][j] = true
       }
     }
+  }
 
-    return tempGrid
+  return tempGrid
 }
