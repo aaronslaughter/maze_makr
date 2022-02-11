f2825c44e6dfc7b43384242dd44a905373675737
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-11 18:37:34 -0500

    added api call to post a maze

--- maze_client/src/components/Save.jsx
+++ maze_client/src/components/Save.jsx
@@ -1,10 +1,17 @@
+import axios from 'axios'
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Button, Modal, Input } from 'react-rainbow-components'
+import { BASE_URL } from '../globals'
 
-const Save = () => {
+const Save = ({user, maze}) => {
 
   const containerStyles = {
     maxWidth: 300,
+  }
+
+  const buttonStyles = {
+    maxWidth: 150,
   }
 
   const [isOpen, toggleOpen] = useState(false)
@@ -16,8 +23,28 @@ const Save = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    // TODO: Axios POST
     toggleOpen(false)
+    const saveMaze = async () => {
+
+      const straightMaze = []
+      for (let row = 0; row < maze.grid.length; row++) {
+        for (let col = 0; col < maze.grid[row].length; col++) {
+          straightMaze.push(maze.grid[row][col])
+        }
+      }
+
+      const data = {
+        name: name, 
+        height: maze.height, 
+        width: maze.width,
+        maze: straightMaze,
+        user_id: user.id
+      }
+
+      await axios.post(`${BASE_URL}/mazes/`, data)
+    }
+    saveMaze()
+    setName('')
   }
 
   return (
@@ -27,18 +54,26 @@ const Save = () => {
         isOpen={isOpen}
         onRequestClose={() => toggleOpen(false)}
         title="Save">
-        <Input
-          id="input-component-1"
-          label="Enter a name for this maze."
-          placeholder="my favorite maze"
-          style={containerStyles}
-          value={name}
-          className="rainbow-m-vertical_x-large rainbow-p-horizontal_medium rainbow-m_auto"
-          onChange={handleChange}/>
-        <Button variant='success' disabled={!name} label='Save' onClick={handleSubmit}></Button>
+        <FlexButtonContainer>
+          <Input
+            id="input-component-1"
+            label="Enter a name for this maze."
+            placeholder="my favorite maze"
+            style={containerStyles}
+            value={name}
+            className="rainbow-m-vertical_x-large rainbow-p-horizontal_medium rainbow-m_auto"
+            onChange={handleChange}/>
+          <Button variant='success' style={buttonStyles} disabled={!name} label='Save' onClick={handleSubmit}></Button>
+        </FlexButtonContainer>
       </Modal>
     </div>
   )
 }
 
 export default Save
+
+const FlexButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+`
