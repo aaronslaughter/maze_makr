8b6eb50def582e3d2ccbdf193f05b9fc4d7ae22e
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-11 15:10:13 -0500

    added depth to the user serializer

--- maze_makr_django/maze_makr/serializers.py
+++ maze_makr_django/maze_makr/serializers.py
@@ -2,34 +2,11 @@ from rest_framework import serializers
 from .models import User, Maze
 
 class MazeSerializer(serializers.HyperlinkedModelSerializer):
-  # user = serializers.HyperlinkedRelatedField(
-  #   view_name='user_detail',
-  #   read_only=True
-  # )
-
-  # user_id = serializers.PrimaryKeyRelatedField(
-  #   queryset=User.objects.all(),
-  #   source='username'
-  # )
-
   class Meta:
     model = Maze
     fields = ('id', 'name', 'height', 'width', 'maze')
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-  # mazes = serializers.HyperlinkedRelatedField(
-  #   view_name='maze_detail',
-  #   many=True,
-  #   read_only=True
-  # )
-
-  mazes = MazeSerializer(many=True)
-
-  # mazes = serializers.PrimaryKeyRelatedField(
-  #   queryset=Maze.objects.all(),
-  #   many=True
-  # )
-
+class UserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
     depth = 1
