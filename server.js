b204af3edf2a2f52bd56a3aa34a3a621b1d70671
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-12 19:04:38 -0500

    express

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
