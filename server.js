06ea1731fd2f0836e6708a51bb00ebe949a9046f
Aaron Slaughter <aslaugh1@gmail.com>
2022-02-12 19:19:33 -0500

    changed client to maze_client

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'maze_client/build')))
   app.get('*', (req, res) => {
-    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    res.sendFile(path.join(`${__dirname}/maze_client/build/index.html`))
   })
 }
 
